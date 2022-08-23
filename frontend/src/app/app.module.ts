7789cd2a1a89feb0b0a990d51f51e4b315b92963
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-08-23 15:29:07 +0530

    code formated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './components/common/nav/nav.component';
-import { FooterComponent } from './components/common/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './components/common/nav/nav.component';
+import {FooterComponent} from './components/common/footer/footer.component';
 import {HomeComponent} from "./components/home/home.component";
-import { RideComponent } from './components/ride/ride.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { DriveComponent } from './components/drive/drive.component';
+import {RideComponent} from './components/ride/ride.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {DriveComponent} from './components/drive/drive.component';
 
 @NgModule({
   declarations: [
@@ -29,4 +29,5 @@ import { DriveComponent } from './components/drive/drive.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
