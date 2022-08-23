2d4143d55d30f5a1f1498bc3648090406eb17517
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-08-23 23:41:56 +0530

    ui divided to topbar component

--- admin-frontend/src/app/app.module.ts
+++ admin-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { TopbarComponent } from './component/common/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
