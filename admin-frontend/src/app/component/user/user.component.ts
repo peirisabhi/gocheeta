51292a7949046ded226f8548b35f137979594edb
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-08-24 17:27:00 +0530

    model close added

--- admin-frontend/src/app/component/user/user.component.ts
+++ admin-frontend/src/app/component/user/user.component.ts
@@ -7,9 +7,7 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  closeResult: string = '';
-
+  
   constructor(private modalService: NgbModal) {
   }
 
@@ -18,23 +16,11 @@ export class UserComponent implements OnInit {
 
   open(content: any) {
     console.log(content)
-    // this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-    //   this.closeResult = `Closed with: ${result}`;
-    // }, (reason) => {
-    //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    // });
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
   }
-  //
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
 
+  close(content:any){
+    this.modalService.dismissAll();
   }
 
 }
