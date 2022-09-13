1b1cd8df943216e580c636d161150cd07344365d
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-09-13 11:29:54 +0530

    citychargedto & city service created

---
+++ admin-service/src/main/java/com/abhishek/gocheeta/adminservice/service/CityChargeService.java
@@ -0,0 +1,10 @@
+package com.abhishek.gocheeta.adminservice.service;
+
+/**
+ * Created by Intellij.
+ * Author: abhishekpeiris
+ * Date: 2022-09-13
+ * Time: 11:25
+ */
+public interface CityChargeService {
+}
