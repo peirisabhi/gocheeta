605dbe6ad921aeb0a42d27c810cefb1cf03530ee
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-09-14 12:35:48 +0530

    driver controller bug fixed

--- admin-service/src/main/java/com/abhishek/gocheeta/adminservice/controller/DriverController.java
+++ admin-service/src/main/java/com/abhishek/gocheeta/adminservice/controller/DriverController.java
@@ -1,6 +1,7 @@
 package com.abhishek.gocheeta.adminservice.controller;
 
 import com.abhishek.gocheeta.adminservice.dto.DriverDto;
+import com.abhishek.gocheeta.adminservice.dto.VehicleCategoryDto;
 import com.abhishek.gocheeta.adminservice.dto.datatable.DataTableRequest;
 import com.abhishek.gocheeta.adminservice.dto.datatable.DataTableResponse;
 import com.abhishek.gocheeta.adminservice.service.impl.DriverServiceImpl;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Intellij.
@@ -31,6 +34,35 @@ public class DriverController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(driverService.saveDriver(driverDto));
     }
+
+    @PutMapping()
+    public ResponseEntity<DriverDto> updateDriver(DriverDto driverDto) {
+        log.info(driverDto.toString());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(driverService.updateDriver(driverDto));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<DriverDto>> getDrivers() {
+        return ResponseEntity.ok(driverService.getDrivers());
+    }
+
+
+    @GetMapping("{driverId}")
+    public ResponseEntity<DriverDto> getDriver(
+            @PathVariable(value = "driverId") int driverId) {
+        return ResponseEntity.ok(driverService.getDriver(driverId));
+    }
+
+
+    @DeleteMapping("{driverId}")
+    public ResponseEntity<DriverDto> removeDriver(
+            @PathVariable(value = "driverId") int driverId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(driverService.removeDriver(driverId));
+    }
+
 
     @PostMapping("/data")
     public DataTableResponse<DriverDto> getDriversForDataTable(@RequestBody DataTableRequest dataTableRequest) {
