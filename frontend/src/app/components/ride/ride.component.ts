38edfaad277d5cc060e20777ee358531e3ccd95e
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-09-19 18:04:53 +0530

    availability validated

--- frontend/src/app/components/ride/ride.component.ts
+++ frontend/src/app/components/ride/ride.component.ts
@@ -5,6 +5,9 @@ import {VehicleCategoryService} from "../../service/vehicle-category-service/veh
 import {VehicleCategory} from "../../model/vehicle-category-model/vehicle-category";
 import {VehicleAvailabilityService} from "../../service/vehicle-availability-service/vehicle-availability.service";
 import {VehicleAvailability} from "../../model/vehicle-availability-model/vehicle-availability";
+import {Booking} from "../../model/booking-model/booking";
+import {formatDate} from "@angular/common";
+import {NotificationService} from "../../service/notification-service/notification.service";
 
 @Component({
   selector: 'app-ride',
@@ -16,15 +19,23 @@ export class RideComponent implements OnInit {
   cities?: City[];
   vehicleCategories?: VehicleCategory[];
   vehicleAvailability: VehicleAvailability = new VehicleAvailability();
+  booking: Booking = new Booking();
+  date?: string;
+
+  price?: string = "0.00"
+  km?: string = "0";
+
 
   constructor(private cityService: CityService,
               private vehicleCategoryService: VehicleCategoryService,
-              private vehicleAvailabilityService: VehicleAvailabilityService) {
+              private vehicleAvailabilityService: VehicleAvailabilityService,
+              private notificationService: NotificationService) {
   }
 
   ngOnInit(): void {
     this.getCities()
     this.getVehicleCategories()
+    console.log(formatDate(new Date(), 'dd/MM/yyyy', 'en'))
   }
 
 
@@ -50,7 +61,13 @@ export class RideComponent implements OnInit {
       && this.vehicleAvailability.to_city != 0
       && this.vehicleAvailability.vehicle_category != 0) {
       this.vehicleAvailabilityService.getAvailability(this.vehicleAvailability).subscribe(data => {
-        console.log()
+        console.log(data)
+
+        if (data.availability) {
+          this.price = data.price;
+        }else {
+          this.notificationService.showError("No riders are currently available", "Booking Unavailable")
+        }
 
       }, error => {
         console.log(error)
--- frontend/src/app/model/booking-model/booking.ts
+++ frontend/src/app/model/booking-model/booking.ts
@@ -1,2 +1,9 @@
+import {formatDate} from "@angular/common";
+
 export class Booking {
+  date = formatDate(new Date(), 'dd/MM/yyyy', 'en');
+  time = new Date();
+  vehicle_category: number = 0;
+  from_city: number = 0;
+  to_city: number = 0;
 }
