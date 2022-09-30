b0c5d0463dfed4730b17bcf67e6b86618dc5d3e0
abhishek peiris <abhishekdananjaya1@yahoo.com>
2022-09-30 22:38:01 +0530

    profile settings

--- frontend/src/app/components/profile-settings/profile-settings.component.ts
+++ frontend/src/app/components/profile-settings/profile-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {StorageService} from "../../service/storage-service/storage.service";
+import {Customer} from "../../model/customer-model/customer";
 
 @Component({
   selector: 'app-profile-settings',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
-  constructor() { }
+  customer: Customer = new Customer();
+
+  constructor(private storageService: StorageService) {
+    this.customer.id = this.storageService.getUser().data.id;
+    this.customer.fname = this.storageService.getUser().data.fname;
+    this.customer.lname = this.storageService.getUser().data.lname;
+    this.customer.dob = this.storageService.getUser().data.dob;
+    this.customer.gender = this.storageService.getUser().data.gender;
+    this.customer.email = this.storageService.getUser().data.email;
+    this.customer.mobile = this.storageService.getUser().data.mobile;
+    this.customer.nic = this.storageService.getUser().data.nic;
+
+    console.log(this.customer)
+  }
 
   ngOnInit(): void {
+
   }
 
 }
